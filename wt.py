8d6cb2836901312af7158ee2806b706301581d0e
raghav9329 <raghav9329@gmail.com>
2018-02-16 12:19:00 +0530

    Update wt.py

--- wt.py
+++ wt.py
@@ -1,9 +1,15 @@
+import unittest
 from selenium import webdriver
-driver=webdriver.Chrome()
-driver.get("http://econpy.pythonanywhere.com/ex/001.html")
-buyers=driver.find_elements_by_xpath("/html[1]/body[1]/div/div")
-prices=driver.find_elements_by_xpath("/html[1]/body[1]/div/span")
-num_items=len(buyers)
-for i in range(num_items):
-    print(buyers[i].text + ": "+ prices[i].text)
-driver.close
+class test(unittest.TestCase):
+    
+    def test(self):
+    	self.driver=webdriver.Chrome()
+    	self.driver.get("http://econpy.pythonanywhere.com/ex/001.html")
+    	buyers=self.driver.find_elements_by_xpath("/html[1]/body[1]/div/div")
+    	prices=self.driver.find_elements_by_xpath("/html[1]/body[1]/div/span")
+    	num_items=len(buyers)
+	for i in range(num_items):
+	    print(buyers[i].text + ": "+ prices[i].text)
+	self.driver.close
+if __name__ == '__main__':
+    unittest.main()
