08e84727cd23fc35de7323962062c3c9527fdf54
Raghvendra <raghvendra@localhost.localdomain>
2018-02-16 17:58:21 +0530

    push

--- wt.py
+++ wt.py
@@ -1,9 +1,30 @@
 from selenium import webdriver
-driver=webdriver.Chrome()
-driver.get("http://econpy.pythonanywhere.com/ex/001.html")
-buyers=driver.find_elements_by_xpath("/html[1]/body[1]/div/div")
-prices=driver.find_elements_by_xpath("/html[1]/body[1]/div/span")
-num_items=len(buyers)
-for i in range(num_items):
-    print(buyers[i].text + ": "+ prices[i].text)
-driver.close
+import unittest
+import os
+
+
+"""
+Python unittest class which demonstrates creating a webdriver instance using environment variables
+populated by the Sauce CI plugins.
+"""
+
+class testSauceWrappers(unittest.TestCase):
+
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        command_executor='http://172.17.0.2:4444/wd/hub',
+        desired_capabilities=DesiredCapabilities.CHROME(chrome_options=options))
+        self.driver = webdriver.Remote(desired_capabilities=desired_capabilities, command_executor=command_executor)
+
+
+    def test_amazon(self):
+        driver = self.driver
+        driver.get("http://amazon.com")
+        print "\rSauceOnDemandSessionID=%s job-name=%s" % (self.driver.session_id, "test_amazon")
+        assert "Amazon.com" in driver.title
+        driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
