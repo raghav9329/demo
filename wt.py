9e1ef2afaa16f218fb04fe453ee1bb1a2e8d2c2d
raghav9329 <raghav9329@gmail.com>
2018-02-16 16:13:01 +0530

    Update wt.py

--- wt.py
+++ wt.py
@@ -1,15 +1,31 @@
-import unittest
 from selenium import webdriver
-class test(unittest.TestCase):
-    
-    def test(self):
-    	self.driver=webdriver.Chrome()
-    	self.driver.get("http://econpy.pythonanywhere.com/ex/001.html")
-    	buyers=self.driver.find_elements_by_xpath("/html[1]/body[1]/div/div")
-    	prices=self.driver.find_elements_by_xpath("/html[1]/body[1]/div/span")
-    	num_items=len(buyers)
-	for i in range(num_items):
-	    print(buyers[i].text + ": "+ prices[i].text)
-	self.driver.close
-if __name__ == '__main__':
+import unittest
+import os
+
+
+"""
+Python unittest class which demonstrates creating a webdriver instance using environment variables
+populated by the Sauce CI plugins.
+"""
+
+class testSauceWrappers(unittest.TestCase):
+
+    def setUp(self):
+        desired_capabilities = {}
+        desired_capabilities['browserName'] = os.environ['SELENIUM_BROWSER']
+        desired_capabilities['version'] = os.getenv('SELENIUM_VERSION', '')
+        desired_capabilities['platform'] = os.environ['SELENIUM_PLATFORM']
+        command_executor = "http://%s:%s@%s:%s/wd/hub" % (os.environ['SAUCE_USER_NAME'], os.environ['SAUCE_API_KEY'], os.environ['SELENIUM_HOST'], os.environ['SELENIUM_PORT'])
+        self.driver = webdriver.Remote(desired_capabilities=desired_capabilities, command_executor=command_executor)
+
+
+    def test_amazon(self):
+        driver = self.driver
+        driver.get("http://amazon.com")
+        print "\rSauceOnDemandSessionID=%s job-name=%s" % (self.driver.session_id, "test_amazon")
+        assert "Amazon.com" in driver.title
+        driver.quit()
+
+
+if __name__ == "__main__":
     unittest.main()
